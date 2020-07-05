98058bc4218d2f9cfb01fdb8a14930bb1a8abdee
Daniel Antonio Nolasco Alvarado <dnieln7@gmail.com>
2020-07-05 16:50:20 -0500

    added CORS configuration

--- src/main/java/com/dnieln7/javaspringapi/SpringAPI.java
+++ src/main/java/com/dnieln7/javaspringapi/SpringAPI.java
@@ -2,6 +2,9 @@ package com.dnieln7.javaspringapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringAPI {
@@ -10,4 +13,14 @@ public class SpringAPI {
         SpringApplication.run(SpringAPI.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/store").allowedOrigins("*");
+                registry.addMapping("/product").allowedOrigins("*");
+            }
+        };
+    }
 }
