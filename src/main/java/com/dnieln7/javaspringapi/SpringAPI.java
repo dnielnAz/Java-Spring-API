c250afac86b7844e497fa46f956e3c4d1fb10c56
dnieln7 <dnieln7@gmail.com>
2020-09-05 10:11:59 -0500

    added swagger documentation

--- src/main/java/com/dnieln7/javaspringapi/SpringAPI.java
+++ src/main/java/com/dnieln7/javaspringapi/SpringAPI.java
@@ -3,8 +3,14 @@ package com.dnieln7.javaspringapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Main class
@@ -33,5 +39,18 @@ public class SpringAPI {
                 registry.addMapping("/dashboard").allowedOrigins("*");
             }
         };
+    }
+
+    @Configuration
+    @EnableSwagger2
+    class SpringFoxConfig {
+        @Bean
+        public Docket api() {
+            return new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .apis(RequestHandlerSelectors.any())
+                    .paths(PathSelectors.any())
+                    .build();
+        }
     }
 }
